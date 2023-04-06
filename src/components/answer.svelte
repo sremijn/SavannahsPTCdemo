2252baa06ea51f729b0a72be05f567fedf61c98b
savannah remijn <savannahremijn@gmail.com>
2023-04-06 19:14:53 +0000

    layout updates

--- src/components/answer.svelte
+++ src/components/answer.svelte
@@ -3,14 +3,21 @@
    export let answer = false;
 </script>
 {#if answer}
-<p>
-   
-   Great job! That was the answer!
-</p>
+<p class="correct">Great job! That was the answer!</p>
 {:else}
-<p>
-   That answer wasn't quite right...
-</p>
+<p class="incorrect">That answer wasn't quite right...</p>
 {/if}
 
-
+<style>
+   .correct, .incorrect{
+font-weight:bold; 
+text-decoration: underline;
+   }
+   
+ .correct  {
+   color:green; 
+ }
+   .incorrect {
+      color: red; 
+   }
+</style>
