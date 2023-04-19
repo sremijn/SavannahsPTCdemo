5eccf38bdf0367d650661664593a34ff0fa8fa38
savannah remijn <savannahremijn@gmail.com>
2023-04-19 00:35:40 +0000

    layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,41 @@
-<div class="background">
-    <div class="layout">
-       .<slot />
-    </div>
-</div>
-<style> 
-.background{
-    padding: 110px;
-    position: absolute; 
-    inset: 0; 
-    background-color:rgb(133, 210, 184); 
-}
-.layout{
-    width: 100%; 
-    max-width: 1024px; 
-    margin: 0 auto; 
-}
+<script>
+	import Score from '../components/score.svelte';
+</script>
 
+<div class="background">
+	<div class="layout">
+		<div class="header"><Score score={0} /></div>
+
+		<div class="content"><slot /></div>
+	</div>
+</div>
+
+<style>
+	.background {
+		padding: 32px;
+		display: grid;
+		position: absolute;
+		inset: 0;
+		background-color: var(--color-purple);
+	}
+	.layout {
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		
+        display: grid;
+		grid-template-rows: auto 1fr;
+		align-items: center;
+	}
+    .header,
+    .content {
+        background-color: aliceblue;
+        border: 4px solid black;
+        padding: 12px 24px; 
+    }
+    .header {
+        display: flex;
+        justify-content: space-between;
+    }
+    
 </style>
